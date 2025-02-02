441fa8f3549739cc11107f46e97d056e07ed01e4
Alex McMurtry <amcmurtry@uchicago.edu>
2025-02-01 20:49:45 -0600

    cross streets is generating good json.

--- street_search.py
+++ street_search.py
@@ -13,59 +13,81 @@ import json
 
 def load_csv(path_to_csv: Path):
     """
-    Read a .csv file from a filepath. 
+    Read a .csv file from a filepath. This is used to load menu_money and 
+    streets data.
 
     Args:
     - path_to_csv (Path): a constructed filepath to the .csv
     Returns a List of rows in dict format
     """
+
+    # Initialize return list, open and read the csv.
     data = []
     with open(path_to_csv, 'r', encoding="utf-8") as filereader:
         csvreader = csv.DictReader(filereader)
-        counter = 0
+        #counter = 0
         for row in csvreader:
-            counter += 1
-            if counter >= 100:
-                break
+            #counter += 1
+            #if counter >= 100:
+                #break
             data.append(row)
         return data
 
 def generate_cross_streets(streets_data):
     """
-    This function generates a dictionary in which the key name indexes a
-    list of cross street names. 
+    This function generates a dictionary in which the street name indexes a
+    list of all cross street names. 
     """
 
     outpath = Path(os.getcwd()) / "data/streets.json"
-
-
-
     street_dict = dict()
 
+    # For each row, make a list containing all segments with same street name
     for row in streets_data:
-        if row['STREET_NAM'] not in street_dict.keys():
-            street_dict[row['STREET_NAM']] = [row]
-        else:
+        if row['STREET_NAM'] in street_dict.keys():
             street_dict[row['STREET_NAM']].append(row)
-
+        else:
+            street_dict[row['STREET_NAM']] = [row]
+            
+    # Write everything to a jsonfile
     with open(outpath, 'w', encoding = 'utf-8') as jsonfile:
         json.dump(street_dict, jsonfile, indent = True)
 
 
 
 def street_searcher(main_street, possible_cross):
+    """
+    This function will access the streets.json file created by 
+    generate_cross_streets. 
+
+    Args:
+    - main_street (str uppercase): This is the main street name. Used as key
+        in streets.json to access a list of cross streets 
+    - possible_cross (str uppercase): This is the name of the cross street 
+        being searched.
+    """
+
+    # Generates the return list and the json filepath for streets.json
     rlist = []
     json_fp = Path(os.getcwd()) / "data/streets.json"
 
+    # Opens streets.json and reads the json
     with open(json_fp, 'r', encoding = "utf-8") as jsonfile:
         streets = json.load(jsonfile)
+
+    # Use the main street name to retrieve the list of cross streets
     cross_list = streets[main_street]
+
+    # Searches each street in the list of cross streets -- both to and from --
+    # for matches, which are appended to the return list
     for street in cross_list:
         tcross = street['T_CROSS'].find(possible_cross)
         fcross = street['F_CROSS'].find(possible_cross)
         print(f"fcross: {fcross}; tcross: {tcross}")
         if tcross or fcross:
             rlist.append(street)
+
+
     return rlist
 
 
@@ -77,11 +99,14 @@ def street_searcher(main_street, possible_cross):
 
 
 if __name__ == "__main__":
+
+
     cwd = os.getcwd()
     streets_fp = Path(cwd) / 'data/streets.csv'
     menu_money = Path(cwd) / 'data/menu_money.csv'
     csv_rows = load_csv(streets_fp)
+
+
+
     menu_money = load_csv(menu_money)
     generate_cross_streets(csv_rows)
-    print(street_searcher("NORTH","MOODY"))
-    
