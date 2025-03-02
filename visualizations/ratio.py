45695a3aa24459761ca40017eb3bd2a57eff05b8
Riley Morrison <rileymorrison@Rileys-Mac-M3.local>
2025-03-02 15:53:53 -0600

    fixed the parks and recs thing in calls_money

--- visualizations/ratio.py
+++ visualizations/ratio.py
@@ -2,7 +2,16 @@ from pathlib import Path
 import os
 import pandas as pd
 
-cwd = Path(os.getcwd())
-file_fp = cwd/'calls_money'
+cwd = Path(os.getcwd()).parent
+file_fp = cwd/'data/calls_money.csv'
 
-read_csv()
+data = pd.read_csv(file_fp)
+data = data[(data['year'] > 2017) & (data['year'] < 2024)]
+
+ward_year = data.groupby(["year",'ward','category']).agg({
+    'calls':"sum",
+    'total_cost':"sum"}).reset_index()
+ward_year["calls_pct"] = ward_year["calls"] / ward_year.groupby(["year","ward"])["calls"].transform("sum") * 100
+ward_year["cost_pct"] = ward_year["total_cost"] / ward_year.groupby(["year","ward"])["total_cost"].transform("sum") * 100
+
+print(ward_year)
