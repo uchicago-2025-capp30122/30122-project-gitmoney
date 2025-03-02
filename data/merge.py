c4cfaf0fc6bb499ae58204b5be4d733460bb4f65
Riley Morrison <rileymorrison@Rileys-Mac-M3.local>
2025-03-02 16:00:55 -0600

    adjusted merge.py

--- data/merge.py
+++ data/merge.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def group_menu():
-    menu = pd.read_csv("menu_money.csv")
+    menlsu = pd.read_csv("menu_money.csv")
     group_menu_project_num = menu.groupby(['year','ward','category']).size()\
     .reset_index(name='num_projects')
     menu['cost'] = menu['cost'].replace(r'\$|,','',regex=True)
