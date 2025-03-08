2975a2cb1929e571d491f7e82284440364e6e24d
Getnet Dejene <getnet@uchicago.edu>
2025-03-07 21:09:19 -0600

    Updated test file

--- tests/test_wiki_scraper.py
+++ tests/test_wiki_scraper.py
@@ -1,24 +1,29 @@
+import pytest
 import processing.wiki_scraper as wiki
 import httpx
-from processing.wiki_scraper import scrape_wikipedia_tables  
+from processing.wiki_scraper import scrape_wikipedia_tables
+
 
 url = 'https://en.wikipedia.org/wiki/List_of_Chicago_alderpersons_since_1923'
 
-
-def test_scrape_wikipedia_tables(url):
-
+def test_scrape_wikipedia_tables():
     # Make an HTTP request
     response = httpx.get(url)
     assert response.status_code == 200, f"Failed to fetch the Wikipedia page. Status code: {response.status_code}"
 
-    # Call the function 
+    # Call the function
     results = scrape_wikipedia_tables(url)
 
-    # Verify the structure of the returned data
-    expected_keys = {"Ward", "Alderperson", "Start Date", "End Date", "Party", "Notes"}
+    # Verify the content of the returned data
+    assert len(results) > 0, "No data was scraped from the Wikipedia page."
     for row in results:
-        assert isinstance(row, dict), "Each item in the list should be a dictionary."
-        assert set(row.keys()) == expected_keys, f"Unexpected keys in the dictionary: {row.keys()}"
+        assert isinstance(row["Ward"], str), "Ward should be a string."
+        assert isinstance(row["Alderperson"], str), "Alderperson should be a string."
+        assert isinstance(row["Start Date"], str), "Start Date should be a string."
+        assert isinstance(row["End Date"], str), "End Date should be a string."
+        assert isinstance(row["Party"], str), "Party should be a string."
+        assert isinstance(row["Notes"], str), "Notes should be a string."
+
 
 def test_find_alder_link():
     resp= httpx.get(url)
@@ -42,3 +47,4 @@ def test_clean_join_wiki_data():
     assert len(alder_data) == 300
     assert len(years) == 6
     assert len(wards) == 50
+    
