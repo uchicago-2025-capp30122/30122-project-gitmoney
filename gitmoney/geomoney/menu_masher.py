e6f10bacc8616c55c160091baf895daefa6752d9
Alex McMurtry <amcmurtry@uchicago.edu>
2025-03-09 14:22:21 -0500

    finished merging, noticed a typo

--- gitmoney/geomoney/menu_masher.py
+++ gitmoney/geomoney/menu_masher.py
@@ -142,7 +142,7 @@ def main():
     cwd = Path(os.getcwd())
     menu_money_fp = cwd / "gitmoney/data/menu_money.csv"
     json_fp = cwd / "gitmoney/data/streets.json"
-    new_money_menu_fp = cwd / "gitmoney/data/new_menu_money.csv")
+    new_money_menu_fp = cwd / "gitmoney/data/new_menu_money.csv"
     menu_money = load_csv(menu_money_fp)
 
     # process the data
