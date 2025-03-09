9e38f38f8cd3d8667ff0eb9a6128fc3b8aa03e9d
Alex McMurtry <amcmurtry@uchicago.edu>
2025-03-09 14:49:19 -0500

    added parser buttons for viz

--- gitmoney/run_and_visualize.py
+++ gitmoney/run_and_visualize.py
@@ -30,35 +30,31 @@ def main(args):
     # process all data 
     if args.all or args.join or not (cwd / 'data/calls_money_pivot_with_alder.csv').exists():
         join_data.join_calls_alders()
-    
+
+    if args.all or args.ratio or not (cwd / 'data/calls_money_pivot_with_alder.csv').exists():
+        create_ratio()
+
     # rewrite the map visualization
-    geomoney.data_visualization.main()
+    if args.all or args.geomoney or not (cwd / 'visualizations/money_calls_scatter.html').exists():
+        geomoney.data_visualization.main()
 
 if __name__ == '__main__':
     cwd = pathlib.Path.cwd()
     if not (cwd / 'data/final_menu_money.csv').exists():
         process_geo_data()
-    #main()
 
     if not (cwd / 'data/calls_money.csv').exists():
         menu_311.raw_311_menu_to_joined_call_money()
 
-#if __name__ == '__main__':
-#    cwd = pathlib.Path.cwd()
     if not (cwd / 'data/all_alderpeople_2018_23.csv').exists():
         wiki.clean_join_wiki_data()
 
     if not (cwd / 'data/calls_money_pivot_with_alder.csv').exists():
         join_data.join_calls_alders()
 
-
-    main()
-
-
-# Creat visualizations
     if not (cwd / 'visualizations/money_calls_scatter.html').exists():
         create_ratio()
-    # command line argument parsing
+        
     parser = argparse.ArgumentParser(
         description='Process and visualize Chicago 311 calls and menu money data'
     )
@@ -69,6 +65,9 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--calls', action='store_true')
     parser.add_argument('-w', '--wiki', action='store_true')
     parser.add_argument('-j', '--join', action='store_true')
+    parser.add_argument('-r', '--ratio', action='store_true')
+    parser.add_argument('-g', '--geomoney', action='store_true')
+    
     
     # parse arguments and run main
     args = parser.parse_args()
